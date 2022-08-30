21146e4d575552a90c3d9a86227b18825392bc10
Eben-123 <ebenezeretsiwah@gmail.com>
2022-08-30 12:01:33 +0000

    Game development Challenge-1

---
+++ Challenge 1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset = new Vector3(35, -1, -7);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    
+    void LateUpdate()
+    {
+        // offset the position of the camera at the side of the plane
+        transform.position = plane.transform.position + offset;
+    }
+}
---
+++ Challenge 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private float speed =40.0f;
+    private float rotationSpeed = 30.0f;
+    private float verticalInput;
+    private float horizontalInput;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    
+    void Update()
+    {
+        // get the user's inputs
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * horizontalInput);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
+       
+    }
+}
